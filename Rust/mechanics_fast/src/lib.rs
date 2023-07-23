8d3f6dd3d4988a0be0f8300b831176763573ea2a
Ilya Andreev <140309026+NeoScripter@users.noreply.github.com>
2023-07-23 03:54:46 +0200

    Implement issue #2: Write Rust method to compute gravitational acceleration from a single object

--- Rust/mechanics_fast/src/lib.rs
+++ Rust/mechanics_fast/src/lib.rs
@@ -1,4 +1,17 @@
+use cgmath::{Vector3, InnerSpace};
+
 #[no_mangle]
 pub extern "C" fn add(a: i32, b: i32) -> i32 {
     a + b
+}
+
+#[no_mangle]
+pub extern "C" fn compute_gravitational_acceleration(displacement: Vector3<f64>, m2: f64) -> Vector3<f64> {
+    const GRAVITATIONAL_CONSTANT: f64 = 6.67430e-11;
+
+    let distance = displacement.magnitude();
+
+    let acceleration = displacement * (GRAVITATIONAL_CONSTANT * m2) / (distance * distance * distance);
+
+    acceleration
 }
