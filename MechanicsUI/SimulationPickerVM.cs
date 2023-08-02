9992cf8acb772fb6ba69d95966abb5e8dc528e22
Ahmil Jilani <hypehuman@users.noreply.github.com>
2023-08-02 11:51:42 +0200

    Split up configuration in UI

--- MechanicsUI/SimulationPickerVM.cs
+++ MechanicsUI/SimulationPickerVM.cs
@@ -1,14 +1,100 @@
-﻿namespace MechanicsUI;
+﻿using MechanicsCore;
+using MechanicsCore.StepConfiguring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MechanicsUI;
 
 public class SimulationPickerVM
 {
-    public PreconfiguredSimulationGroupVM PreconfiguredVM { get; }
-    public ConfigurableSimulationGroupVM ConfigurableVM { get; }
+    public IReadOnlyList<string> PreconfigNames { get; } = typeof(PreconfiguredSimulations).GetProperties().Select(p => p.Name).ToArray();
+    public IReadOnlyList<string> ScenarioNames { get; } = typeof(Simulations).GetMethods().Where(m => m.ReturnType == typeof(Simulation)).Select(p => p.Name).ToArray();
+
     public bool AutoStart { get; set; }
+
+    #region Init Config
+
+    public string SelectedScenarioName { get; set; }
+
+    public string SeedString { get; set; } = string.Empty;
+    /* make private */
+    public int? GetSeed() => TryParseSeed(SeedString, out var seed) ? seed : null;
+
+    #endregion
+
+    #region Step Config
+
+    private static readonly IReadOnlyList<GravityType> sGravityTypes = Enum.GetValues<GravityType>();
+    public static IReadOnlyList<GravityType> GravityTypes => sGravityTypes;
+    public GravityType GravityConfig { get; set; }
+
+    private static readonly IReadOnlyList<CollisionType> sCollisionTypes = Enum.GetValues<CollisionType>();
+    public static IReadOnlyList<CollisionType> CollisionTypes => sCollisionTypes;
+    public CollisionType CollisionConfig { get; set; }
+
+    #endregion
 
     public SimulationPickerVM()
     {
-        PreconfiguredVM = new PreconfiguredSimulationGroupVM(this);
-        ConfigurableVM = new ConfigurableSimulationGroupVM(this);
+        SelectedScenarioName = ScenarioNames[0];
+    }
+
+    /// <summary>
+    /// TODO: Replace with a method that sets the config values instead of starting a new simulation
+    /// </summary>
+    public SimulationVM StartPreconfiguredSimulation(string name)
+    {
+        var invoked = typeof(PreconfiguredSimulations).GetStaticPropertyValue(name)
+            ?? throw new NullReferenceException($"'{name}' was null");
+        var sim = (Simulation)invoked;
+        return new SimulationVM(sim, name)
+        {
+            IsAutoLeaping = AutoStart,
+        };
+    }
+
+    public SimulationVM StartSimulation()
+    {
+        string name = SelectedScenarioName;
+        var invoked = typeof(Simulations).InvokeStatic(name, GetSeed())
+            ?? throw new NullReferenceException($"'{name}' was null");
+        var sim = (Simulation)invoked;
+        SetStepConfiguration(sim);
+        return new SimulationVM(sim, name)
+        {
+            IsAutoLeaping = AutoStart,
+        };
+    }
+
+    private void SetStepConfiguration(Simulation sim)
+    {
+        // TODO: Configure step time
+        // TODO: Configure steps per leap
+        sim.StepConfig.GravityConfig = GravityConfig;
+        // TODO: Configure buoyancy ratio if relevant
+        sim.StepConfig.CollisionConfig = CollisionConfig;
+        // TODO: Configure drag coefficient if relevant
+    }
+
+    public static bool TryParseSeed(string seedString, out int? seed)
+    {
+        if (string.IsNullOrWhiteSpace(seedString))
+        {
+            // Null is intentional; seed will be random.
+            seed = null;
+            return true;
+        }
+
+        if (int.TryParse(seedString, out var parsedSeed))
+        {
+            // Got an integer; seed will be fixed to the specified value.
+            seed = parsedSeed;
+            return true;
+        }
+
+        // Parsing failed.
+        seed = null;
+        return false;
     }
 }
--- MechanicsUI/SimulationPickerView.xaml.cs
+++ MechanicsUI/SimulationPickerView.xaml.cs
@@ -1,11 +1,55 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace MechanicsUI;
 
 public partial class SimulationPickerView : UserControl
 {
+    public SimulationPickerVM? ViewModel => DataContext as SimulationPickerVM;
+
     public SimulationPickerView()
     {
         InitializeComponent();
+    }
+
+    private void PreconfiguredButton_Click(object sender, RoutedEventArgs e)
+    {
+        var vm = ViewModel;
+        if (vm == null)
+            return;
+
+        var preconfigName = (string)((FrameworkElement)sender).DataContext;
+
+        // TODO: Instead of starting the simulation, just set the config values.
+        var simVm = vm.StartPreconfiguredSimulation(preconfigName);
+        ShowSimWindow(simVm);
+    }
+
+    private void LaunchButton_Click(object sender, RoutedEventArgs e)
+    {
+        var vm = ViewModel;
+        if (vm == null)
+            return;
+
+        var simVm = vm.StartSimulation();
+        ShowSimWindow(simVm);
+    }
+
+    private static void ShowSimWindow(SimulationVM simVm)
+    {
+        var simWindow = new Window
+        {
+            Title = simVm.Title,
+            Content = new SimulationView
+            {
+                DataContext = simVm
+            },
+        };
+        simWindow.Closed += delegate
+        {
+            // Stop the simulation from running in the background
+            simVm.IsAutoLeaping = false;
+        };
+        simWindow.Show();
     }
 }
