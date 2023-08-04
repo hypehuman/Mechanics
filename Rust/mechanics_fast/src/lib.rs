77e36312050248579634bfd5badc54743bc6c48b
Ahmil Jilani <hypehuman@users.noreply.github.com>
2023-08-04 08:37:56 +0200

    Adding unit test for test_compute_gravitational_acceleration

--- Rust/mechanics_fast/src/lib.rs
+++ Rust/mechanics_fast/src/lib.rs
@@ -10,3 +10,30 @@ pub extern "C" fn compute_gravitational_acceleration(displacement: Vector3<f64>,
 
     acceleration
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cgmath::assert_relative_eq;
+
+    #[test]
+    fn test_compute_gravitational_acceleration() {
+        // acceleration of earth due to sun's gravity
+        test_compute_gravitational_acceleration_once(
+            Vector3::new(1.4960e11, 0.0, 0.0),
+            1.98847e30,
+            Vector3::new(5.9301e-3, 0.0, 0.0)
+        );
+        // acceleration of sun due to earth's gravity
+        test_compute_gravitational_acceleration_once(
+            -Vector3::new(1.4960e11, 0.0, 0.0),
+            5.9722e24,
+            -Vector3::new(1.7815e-8, 0.0, 0.0)
+        );
+    }
+    
+    fn test_compute_gravitational_acceleration_once(displacement: Vector3<f64>, m2: f64, expected: Vector3<f64>) {
+        let actual = compute_gravitational_acceleration(displacement, m2);
+        assert_relative_eq!(expected, actual, max_relative = 0.001);
+    }
+}
