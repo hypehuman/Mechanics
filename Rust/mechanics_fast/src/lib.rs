b5954d1bccb6d26cb5dbb95628c63330d048374a
Ilya Andreev <140309026+NeoScripter@users.noreply.github.com>
2023-08-05 22:52:05 +0800

    Update lib.rs
    Reverted to the workinng version of the computer gravitational acceleration and fixed signature

--- Rust/mechanics_fast/src/lib.rs
+++ Rust/mechanics_fast/src/lib.rs
@@ -1,19 +1,16 @@
 use cgmath::{InnerSpace, Vector3};
 
-fn compute_gravitational_acceleration(displacement: Vector3<f32>, mass: f32) -> Vector3<f32> {
-    let g: f32 = 9.81;
+#[no_mangle]
+pub extern "C" fn compute_gravitational_acceleration(displacement: Vector3<f64>, m2: f64) -> Vector3<f64> {
+    const GRAVITATIONAL_CONSTANT: f64 = 6.67430e-11;
 
-    let distance_squared = displacement.magnitude2();
+    let distance = displacement.magnitude();
 
-    if distance_squared > 0.0 {
-        let magnitude = g * mass / distance_squared.sqrt();
-        displacement.normalize() * magnitude
-    } else {
-        Vector3::new(0.0, 0.0, 0.0)
-    }
+    let acceleration = displacement * (GRAVITATIONAL_CONSTANT * m2) / (distance * distance * distance);
+    acceleration
 }
 
-fn compute_acceleration(masses: &[f32], positions: &[Vector3<f32>], index_of_self: usize) -> Vector3<f32> {
+fn compute_acceleration(masses: &[f64], positions: &[Vector3<f64>], index_of_self: usize) -> Vector3<f64> {
     let mut acceleration = Vector3::new(0.0, 0.0, 0.0);
 
     for i in 0..masses.len() {
