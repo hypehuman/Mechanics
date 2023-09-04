2df587b9817c88a633febabeb76af035cc28f0b3
Ahmil Jilani <hypehuman@users.noreply.github.com>
2023-09-04 19:08:57 +0200

    WIP: Grab solar system data from NASA Horizons API

---
+++ HorizonsToMechanicsConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using HorizonsToMechanics;
+
+namespace HorizonsToMechanicsConsole;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        foreach (var x in Class1.IterateObjects())
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
