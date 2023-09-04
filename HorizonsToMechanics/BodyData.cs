b7834e7d0cea07ef1e8706b03a2acb1740e73475
Ahmil Jilani <hypehuman@users.noreply.github.com>
2023-09-04 19:08:57 +0200

    Writing code that will query and save solar system body data (incl. name, mass, radius) from NASA Horizons, as well as get the position and velocity at a particular date/time. NASA supports getting a range of time points (and "ephemeris"), but we only want one for now, which we will eventually use as the initial configuration for a Simulation.

---
+++ HorizonsToMechanics/BodyData.cs
@@ -0,0 +1,6 @@
+﻿namespace HorizonsToMechanics;
+
+public record BodyData
+(
+    int ID
+);
---
+++ HorizonsToMechanicsConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using HorizonsToMechanics;
+
+namespace HorizonsToMechanicsConsole;
+
+internal class Program
+{
+    private static readonly CancellationTokenSource sCancelSource = new();
+
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Downloading Horizons data. Ctrl+C to end");
+
+        Console.CancelKeyPress += (sender, eventArgs) =>
+        {
+            Console.WriteLine("Cancel event triggered");
+            sCancelSource.Cancel();
+            eventArgs.Cancel = true;
+        };
+
+        Console.WriteLine("Body data from NASA Horizons:");
+        var jsonDir = "../../../../HorizonsToMechanics/Downloaded/json";
+        var txtDir = "../../../../HorizonsToMechanics/Downloaded/txt";
+        await foreach (var bodyData in new BodyDataEnumerable(jsonDir, txtDir).WithCancellation(sCancelSource.Token))
+        {
+            if (!sCancelSource.IsCancellationRequested)
+            {
+                Console.WriteLine($"Object {bodyData.ID}: Success!");
+            }
+        }
+    }
+}
