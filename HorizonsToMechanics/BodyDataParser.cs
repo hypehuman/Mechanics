3423a861791ab6b76cf8028e67ea32d54cbf77f2
Ahmil Jilani <hypehuman@users.noreply.github.com>
2023-09-07 22:32:29 +0200

    Try fix 501 but it breaks 10

--- HorizonsToMechanics/BodyDataParser.cs
+++ HorizonsToMechanics/BodyDataParser.cs
@@ -214,16 +214,19 @@ internal static partial class BodyDataParser
     // The mass of 1.08e16 kg is represented as "Mass (10^20 kg )        =  1.08 (10^-4)"
     private static readonly Regex sMassPattern = MassPattern();
     [GeneratedRegex(
-        """(?<!Rocky core )\bMass\b(?! ratio| of atmosphere| layers|-energy conv rate),?\s*(?<units>[^=]*)\s*\=\s*(?<value>[\S]+\s*(\([^)]*\))?)""",
+        """(?<!Rocky core )\bMass\b(?! ratio| of atmosphere| layers|-energy conv rate),?\s*(?<units>[^=]*)\s*\=\s*(?<value>[\S]+\s*(\([^)]*\))?(?:[^\n\s]|\s(!:[A-Z])))""",
         RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.ExplicitCapture
     )]
     private static partial Regex MassPattern();
 
     // |\) is to not get confused by 852 Wladilena (A916 GM)
     // 2013 Siding Spring has the units explained in its Comet Physical line "Comet physical (GM= km^3/s^2; RAD= km):  "
+    // End with """(?:[^\n\s]|\s(!:[A-Z]))""", which keeps matching anything until we find a space followed by a capital letter,
+    //     which probably signals the start of a new property name.
+    //     E.g.: GM (km^3/s^2)          = 5959.9155+- 0.004 Geometric Albedo  = 0.63  +- 0.02
     private static readonly Regex sGmPattern = GmPattern();
     [GeneratedRegex(
-        """(?<!Comet physical \()\bGM\b(?! 1-sigma|\)),?\s*(?<units>[^=]*)\s*\=\s*(?<value>[\S]+\s*(\([^)]*\))?)""",
+        """(?<!Comet physical \()\bGM\b(?! 1-sigma|\)),?\s*(?<units>[^=]*)\s*\=\s*(?<value>[\S]+\s*(\([^)]*\))?(?:[^\n\s]|\s(!:[A-Z])))""",
         RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.ExplicitCapture
     )]
     private static partial Regex GmPattern();
