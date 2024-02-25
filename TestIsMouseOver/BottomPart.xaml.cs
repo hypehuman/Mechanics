d8c9d83585af60422581ef4d266d62903205fa2f
Ahmil Jilani <hypehuman@users.noreply.github.com>
2024-02-25 10:36:26 +0100

    Attempting to reproduce the bug (see below) in a new project, but cannot reproduce. Interestingly, the "Project Type GUID" (in the *.sln file) is different from the MechanicsUI project. I expected them to be the same because they're both WPF apps.
    Bug description: The by-distance names sometimes remain visible when the mouse leaves the canvas. This seems to happen when the mouse was hovering over the by-distance names before it left, especially if (1) the mouse moves slowly to the left and the cursor changes to the window resizer, or (2) the mouse moves quickly upwards onto the controls/info panel.

---
+++ TestIsMouseOver/BottomPart.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace TestIsMouseOver;
+
+partial class BottomPart
+{
+    public BottomPart()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnMouseMove(MouseEventArgs e)
+    {
+        base.OnMouseMove(e);
+
+        (DataContext as BottomPartViewModel)?.AddItem("OnMouseMove: " + e.GetPosition(this));
+    }
+
+    protected override void OnMouseLeave(MouseEventArgs e)
+    {
+        base.OnMouseLeave(e);
+
+        (DataContext as BottomPartViewModel)?.AddItem("OnMouseLeave");
+    }
+}
+
+public class BottomPartViewModel
+{
+    public ObservableCollection<object> Items { get; } = new ObservableCollection<object>
+    {
+        "Initial Item 1",
+        "Initial Item 2",
+        "Initial Item 3",
+    };
+
+    internal void AddItem(object item)
+    {
+        Items.RemoveAt(Items.Count - 1);
+        Items.Insert(0, item);
+    }
+}
