e9327d51f4dde231b74d93b983697d95fa8b27b0
Ahmil Jilani <hypehuman@users.noreply.github.com>
2024-10-07 15:51:25 +0200

    Using zColorWheel to try out a new color space that should be perceptually uniform.

--- MechanicsCore/RingColorSpace.cs
+++ MechanicsCore/RingColorSpace.cs
@@ -1,4 +1,5 @@
 ﻿using GuiByReflection.Models;
+using Wacton.Unicolour;
 
 namespace MechanicsCore;
 
@@ -18,6 +19,15 @@ public enum RingColorSpace
 
     [GuiHelp("Returns a color with the given sRGB hue angle with a fixed radiance of 1.0 out of 3.0.")]
     RgbFixedRadiance,
+
+    [GuiHelp(
+        "Returns a color in the CAM16-UCS color space with a carefully selected fixed J and fixed (A^2+B^2).",
+        "This has some properties that I find useful:",
+        "(1) As you vary the hue angle, the perceived color change varies in direct proportion with it.",
+        "(2) As you vary the hue angle, the percieved brigtness and saturation do not change.",
+        "(3) The entire space fits inside sRGB."
+    )]
+    Cam16UcsRing,
 }
 
 public static class CircularColorSpaces
@@ -26,6 +36,7 @@ public static class CircularColorSpaces
     {
         RingColorSpace.RgbSaturated => RgbSaturated,
         RingColorSpace.RgbFixedRadiance => RgbFixedRadiance,
+        RingColorSpace.Cam16UcsRing => Cam16UcsRing,
         _ => throw Utils.OutOfRange(nameof(colorSpace), colorSpace)
     };
 
@@ -102,6 +113,48 @@ public static class CircularColorSpaces
         }
     }
 
+    private static BodyColor Cam16UcsRing(double hue_0_1)
+    {
+        // These constants were selected to maximize the radius without stepping outside of the sRGB color space,
+        // I.e., maximize radius_0_50 while keeping RgbDoubles_0_1.FitsInSrgb() true for all hue angles.
+        // This code for this optimization is in repo SierMeng, and includes visual outputs of the candidate color spaces.
+        // I experimented with letting centerA and centerB wander around,
+        // I which did find rings with larger radii.
+        // However, it made the ring space vary in colorfulness;
+        // i.e., some parts of the ring looked grayer than others.
+        // Therefore, to keep uniform colorfulness,
+        // we pin the center of the ring at (A=0, B=0).
+        const double j_0_100 = 72.428;
+        const double centerA_neg50_50 = 0.0;
+        const double centerB_neg50_50 = 0.0;
+        const double radius_0_50 = 26.2557;
+
+        return Cam16UcsRing(hue_0_1, j_0_100, centerA_neg50_50, centerB_neg50_50, radius_0_50);
+    }
+
+    public static BodyColor Cam16UcsRing(double hue_0_1, double j_0_100, double centerA_neg50_50, double centerB_neg50_50, double radius_0_50)
+    {
+        var rgb_0_1 = Cam16UcsRing_0_1(hue_0_1, j_0_100, centerA_neg50_50, centerB_neg50_50, radius_0_50);
+        var result = rgb_0_1.ToBodyColor();
+        return result;
+    }
+
+    public static RgbDoubles_0_1 Cam16UcsRing_0_1(double hue_0_1, double j_0_100, double centerA_neg50_50, double centerB_neg50_50, double radius_0_50)
+    {
+        var hueRad = hue_0_1 * Math.Tau;
+        var a_neg50_50 = centerA_neg50_50 + radius_0_50 * Math.Cos(hueRad);
+        var b_neg50_50 = centerB_neg50_50 + radius_0_50 * Math.Sin(hueRad);
+
+        var rgb_0_1 = Cam16Ucs(j_0_100, a_neg50_50, b_neg50_50);
+        return rgb_0_1;
+    }
+
+    private static RgbDoubles_0_1 Cam16Ucs(double j_0_100, double a_neg50_50, double b_neg50_50)
+    {
+        var cam16ucs = new Unicolour(ColourSpace.Cam16, j_0_100, a_neg50_50, b_neg50_50);
+        return RgbDoubles_0_1.FromUnicolour(cam16ucs);
+    }
+
     public readonly record struct RgbDoubles_0_1(double R = 0, double G = 0, double B = 0)
     {
         public BodyColor ToBodyColor() => new(
@@ -109,6 +162,20 @@ public static class CircularColorSpaces
             G: Double_0_1ToByte_0_255(G),
             B: Double_0_1ToByte_0_255(B)
         );
+
+        public static RgbDoubles_0_1 FromUnicolour(Unicolour uni)
+        {
+            var rgb = uni.Rgb;
+            return new RgbDoubles_0_1(rgb.R, rgb.G, rgb.B);
+        }
+
+        public bool FitsInSrgb()
+        {
+            return
+                0 <= R && R <= 1 &&
+                0 <= G && G <= 1 &&
+                0 <= B && B <= 1;
+        }
     }
 
     private static byte Double_0_1ToByte_0_255(double double_0_1)
